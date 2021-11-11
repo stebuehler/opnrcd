a91942ec82eb6e078169d19b594a7117618cff33
stebuehler <stibe.buehler@gmail.com>
2021-11-11 22:20:36 +0100

    Second app (v1_1) with scatterplot and multi-select for years

--- dash_v1_1.py
+++ dash_v1_1.py
@@ -3,17 +3,18 @@ import pandas as pd
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import numpy as np
 
 opnrcd_full_df = pd.read_csv('source_data/OPNRCD_alltime_stats.csv')
 opnrcd_df = opnrcd_full_df[opnrcd_full_df["Strophe?"]]  # Strophen only
+opnrcd_df = opnrcd_df.astype({"Jahr": str})
+
 
 def extract_years(df):
-    years = df['Jahr'].unique()
-    years.sort()
-    years = np.append(years, np.array(['All']))
-    years = years[::-1]
-    return years
+    years_list = df['Jahr'].unique()
+    years_list.sort()
+    years_list = years_list[::-1]
+    return years_list
+
 
 years = extract_years(opnrcd_df)
 
@@ -22,7 +23,7 @@ server = app.server
 
 app.layout = html.Div([
     html.Div([dcc.Dropdown(id='year-select', options=[{'label': i, 'value': i} for i in years],
-                           value='All', style={'width': '140px'})]),
+                           multi=True, value=years, style={'width': '140px'})]),
     dcc.Graph('scatterplot', config={'displayModeBar': False})])
 
 
@@ -30,11 +31,11 @@ app.layout = html.Div([
     Output('scatterplot', 'figure'),
     [Input('year-select', 'value')]
 )
-def update_graph(year):
+def update_graph(years):
     import plotly.express as px
-    df_for_plot = opnrcd_df[opnrcd_df['Jahr'] == int(year)] if year != 'All' else opnrcd_df
+    df_for_plot = opnrcd_df[opnrcd_df['Jahr'].isin(years)]
     fig = px.scatter(df_for_plot, x='Künstlerische Relevanz (1-10)', y='Musikalische Härte (1-10)',
-                      size='Dauer (s)', color='Jahr')
+                     size='Dauer (s)', color='Jahr')
     fig.update_layout(transition_duration=200)
     return fig
 
