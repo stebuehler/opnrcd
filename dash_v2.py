2f46f454e936cd383bde97273bb59527c8bf0cd1
stebuehler <stibe.buehler@gmail.com>
2021-11-11 22:49:03 +0100

    third app with scatterplot and freely selectable x and y axes

--- dash_v2.py
+++ dash_v2.py
@@ -3,23 +3,14 @@ import pandas as pd
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import numpy as np
 
 opnrcd_full_df = pd.read_csv('source_data/OPNRCD_alltime_stats.csv')
 opnrcd_df = opnrcd_full_df[opnrcd_full_df["Strophe?"]]  # Strophen only
+opnrcd_df = opnrcd_df.astype({"Jahr": str})
 
 app = dash.Dash(__name__)
 server = app.server
 
-
-def extract_years(df):
-    year_list = df['Jahr'].unique()
-    year_list.sort()
-    year_list = np.append(year_list, np.array(['All']))
-    year_list = year_list[::-1]
-    return year_list
-
-years = extract_years(opnrcd_df)
 numerical_variables = ['Künstlerische Relevanz (1-10)',
                        'Musikalische Härte (1-10)',
                        'Tanzbarkeit (1-10)',
@@ -28,27 +19,26 @@ numerical_variables = ['Künstlerische Relevanz (1-10)',
                        'Weirdness (1-8)']
 
 app.layout = html.Div([
-    html.Div([dcc.Dropdown(id='year-select', options=[{'label': i, 'value': i} for i in years],
-                           value='All', style={'width': '140px'})]),
-    html.Div([dcc.Dropdown(id='x-axis-select', options=[{'label': i, 'value': i} for i in numerical_variables],
-                               value='Künstlerische Relevanz (1-10)', style={'width': '180px'})]),
-    html.Div([dcc.Dropdown(id='y-axis-select', options=[{'label': i, 'value': i} for i in numerical_variables],
-                                   value='Musikalische Härte (1-10)', style={'width': '180px'})]),
+    html.Div([
+        html.Label(['x-axis:'], style={'font-weight': 'bold', "text-align": "center"}),
+        dcc.Dropdown(id='x-axis-select', options=[{'label': i, 'value': i} for i in numerical_variables],
+                           value='Künstlerische Relevanz (1-10)', style={'width': '50%'}),
+        html.Label(['y-axis:'], style={'font-weight': 'bold', "text-align": "center"}),
+        dcc.Dropdown(id='y-axis-select', options=[{'label': i, 'value': i} for i in numerical_variables],
+                           value='Musikalische Härte (1-10)', style={'width': '50%'})
+    ]),
     dcc.Graph('scatterplot', config={'displayModeBar': False})])
 
 
 @app.callback(
     Output('scatterplot', 'figure'),
-    [Input('year-select', 'value')]
-     # Input('x-axis-select', 'value'),
-     # Input('y-axis-select', 'value')]
-)
-def update_graph(year):
+    Input('x-axis-select', 'value'),
+    Input('y-axis-select', 'value'))
+def update_graph(x_axis_name, y_axis_name):
     import plotly.express as px
-    df_for_plot = opnrcd_df[opnrcd_df['Jahr'] == int(year)] if year != 'All' else opnrcd_df
-    return px.scatter(df_for_plot, x='Künstlerische Relevanz (1-10)', y='Musikalische Härte (1-10)',
-                      size='Dauer (s)', color='Jahr')
-
+    fig = px.scatter(opnrcd_df, x=x_axis_name, y=y_axis_name, size='Dauer (s)', color='Jahr')
+    fig.update_layout(transition_duration=200)
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=False)
