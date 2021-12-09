3b784a9775628354221bf168cc97f01430301e15
stebuehler <stibe.buehler@gmail.com>
2021-12-09 07:16:09 +0100

    Added functionality to show / hide filters depending on tab (see treemap tab) Needs refactoring if possible.

--- dash_multitab.py
+++ dash_multitab.py
@@ -76,12 +76,12 @@ app.layout = html.Div([
             id='year-select', options=[{'label': i, 'value': i} for i in all_years],
             multi=True, value=all_years
         ),
-        html.Label(['x-axis:'], style={'font-weight': 'bold', "text-align": "left"}),
+        html.Label(['x-axis:'], style={'font-weight': 'bold', "text-align": "left"}, id='x-axis-select-label'),
         dcc.Dropdown(
             id='x-axis-select', options=[{'label': i, 'value': i} for i in NUMERICAL_VARIABLES],
             value=NUMERICAL_VARIABLES[0]
         ),
-        html.Label(['y-axis:'], style={'font-weight': 'bold', "text-align": "left"}),
+        html.Label(['y-axis:'], style={'font-weight': 'bold', "text-align": "left"}, id='y-axis-select-label'),
         dcc.Dropdown(
             id='y-axis-select', options=[{'label': i, 'value': i} for i in NUMERICAL_VARIABLES],
             value=NUMERICAL_VARIABLES[1]
@@ -92,6 +92,10 @@ app.layout = html.Div([
 
 @app.callback(
     Output('tabs-content-graph', 'children'),
+    Output('x-axis-select', 'style'),
+    Output('x-axis-select-label', 'style'),
+    Output('y-axis-select', 'style'),
+    Output('y-axis-select-label', 'style'),
     Input('tabs-graph', 'value'),
     Input('x-axis-select', 'value'),
     Input('y-axis-select', 'value'),
@@ -99,33 +103,45 @@ app.layout = html.Div([
 )
 def render_content(tab, x_axis_name, y_axis_name, years):
     df = opnrcd_df[opnrcd_df['Jahr'].isin(years)]
+    # standard filter display options. Change them to "None" for the tabs that should not show them
+    x_axis_select_display = {'display': 'Block'}
+    y_axis_select_display = {'display': 'Block'}
+    x_axis_select_label_display = {'display': 'Block'}
+    y_axis_select_label_display = {'display': 'Block'}
+    # forking into tabs
     if tab == 'tab-1-graph':
         fig = px.scatter(df, x=x_axis_name, y=y_axis_name, size='Dauer (s)', color='Jahr')
         fig.update_layout(transition_duration=200)
-        return html.Div([dcc.Graph(id='graph-1-tabs', figure=fig)])
+        return html.Div([dcc.Graph(id='graph-1-tabs', figure=fig)]), x_axis_select_display, x_axis_select_label_display, y_axis_select_display, y_axis_select_label_display
     elif tab == 'tab-2-graph':
         # TODO option to select value of heatmap: count / duration
         # heatmap_df = df.groupby([x_axis_name, y_axis_name]).count()['Jahr'].unstack(x_axis_name).fillna(0.0)  # count
         heatmap_df = df.groupby([x_axis_name, y_axis_name]).sum()['Dauer (s)'].unstack(x_axis_name).fillna(0.0)  # duration
         fig = px.imshow(heatmap_df)
         fig.update_layout(transition_duration=200)
-        return html.Div([dcc.Graph(id='graph-2-tabs', figure=fig)])
+        return html.Div([dcc.Graph(id='graph-2-tabs', figure=fig)]), x_axis_select_display, x_axis_select_label_display, y_axis_select_display, y_axis_select_label_display
     elif tab == 'tab-3-graph':
         corr_df = df[NUMERICAL_VARIABLES].dropna().corr()
         fig = px.imshow(corr_df)
         # TODO find a way to show correlation numbers in heat map
         # fig.update_traces(textposition='inside')
         fig.update_layout(transition_duration=200)
-        return html.Div([dcc.Graph(id='graph-3-tabs',figure=fig)])
+        return html.Div([dcc.Graph(id='graph-3-tabs',figure=fig)]), x_axis_select_display, x_axis_select_label_display, y_axis_select_display, y_axis_select_label_display
     elif tab == 'tab-4-graph':
         fig = get_time_series_fig(x_axis_name, y_axis_name)
-        return html.Div([dcc.Graph(id='graph-4-tabs',figure=fig)])
+        return html.Div([dcc.Graph(id='graph-4-tabs',figure=fig)]), x_axis_select_display, x_axis_select_label_display, y_axis_select_display, y_axis_select_label_display
     elif tab == 'tab-5-graph':
         # TODO Let user chose if grouping by Country or different attribute (Baujahr etc.)
         # TODO choice whether to include skits or not (currently not)
+        # filters
+        x_axis_select_display = {'display': 'None'}
+        y_axis_select_display = {'display': 'None'}
+        x_axis_select_label_display = {'display': 'None'}
+        y_axis_select_label_display = {'display': 'None'}
+        # graph
         fig = px.treemap(df, path=['Kontinent', 'Nationalität', 'Künstler'], values='Dauer (s)')
         fig.data[0].hovertemplate = '%{label}<br>%{value}'
-        return html.Div([dcc.Graph(id='graph-5-tabs', figure=fig)])
+        return html.Div([dcc.Graph(id='graph-5-tabs', figure=fig)]), x_axis_select_display, x_axis_select_label_display, y_axis_select_display, y_axis_select_label_display
 
 
 if __name__ == '__main__':
