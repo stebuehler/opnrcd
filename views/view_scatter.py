9dc0c6251504c85a960bd0cde612356507bad62d
MathiasBrucherseifer <mathias.brucherseifer@gmail.com>
2022-01-15 13:43:06 +0100

    static x & y ranges in heatmap, show Stropheninfo in tooltip for scatter view

--- views/view_scatter.py
+++ views/view_scatter.py
@@ -13,5 +13,5 @@ class ViewScatter(AbstractView):
         x_axis_name = kwargs['x-axis']
         y_axis_name = kwargs['y-axis']
         self.df = opnrcd_df[opnrcd_df['Jahr'].isin(years)]
-        self.fig = px.scatter(self.df, x=x_axis_name, y=y_axis_name, size='Dauer (s)', color='Jahr')
+        self.fig = px.scatter(self.df, x=x_axis_name, y=y_axis_name, size='Dauer (s)', color='Jahr', hover_data=['Künstler', 'Titel'])
         self.fig.update_layout(transition_duration=200)
