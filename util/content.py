1693c75a66dbfa857deec654112bb81e8bc9e7f9
stebuehler <stibe.buehler@gmail.com>
2022-01-28 06:32:32 +0100

    small reformatting

--- util/content.py
+++ util/content.py
@@ -6,9 +6,9 @@ def offcanvas_content():
         Die Bewertung der Strophen in den sechs Kategorien wurde stets in alkoholisiertem Zustand im Reinraum von geschultem Personal (OPNRCDKMT) durchgeführt; und ist ISO-zertifiziert. Alle Kategorien weisen ganzzahlige Wert zwischen eins und zehn auf. Ausser die Kategorie Weirdness, welche sich von eins bis acht erstreckt.
         
         #### Impressum
-        OPNRBZNZ GmbH
-        Ankerstrasse 12
-        0000 T0tzball0nienburg
-        Ankerland
+        OPNRBZNZ GmbH  
+        Ankerstrasse 12  
+        0000 T0tzball0nienburg  
+        Ankerland  
         ''')
     return content
