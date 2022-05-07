e9016b759db166a34d51ae15b5b532f4a86e5c4f
stebuehler <stibe.buehler@gmail.com>
2022-05-07 23:22:19 +0200

    add Jahr and Baujahr as options in scatter x and y

--- views/view_scatter.py
+++ views/view_scatter.py
@@ -8,8 +8,8 @@ class ViewScatter(AbstractView):
         self.label = 'Streudiagramm'
         self.value = self.label + '-graph'
         self.add_display_option('Gruppierung', ['Jahr', 'Nationalität', 'Kontinent', 'Sprache', 'Baujahr', 'Baujahr Jahrzehnt', 'Künstler', 'Strophentitel'])
-        self.add_display_option('x-Achse', NUMERICAL_VARIABLES + ['Startzeit (s)', 'Dauer (m)'])
-        self.add_display_option('y-Achse', NUMERICAL_VARIABLES + ['Startzeit (s)', 'Dauer (m)'], default_selection=1)
+        self.add_display_option('x-Achse', NUMERICAL_VARIABLES + ['Startzeit (s)', 'Dauer (m)', 'Jahr', 'Baujahr'])
+        self.add_display_option('y-Achse', NUMERICAL_VARIABLES + ['Startzeit (s)', 'Dauer (m)', 'Jahr', 'Baujahr'], default_selection=1)
         self.add_display_option('Farbe', NUMERICAL_VARIABLES + ['Startzeit (s)', 'Dauer (m)', 'Jahr', 'Baujahr'], default_selection=2)
         self.add_display_option('Beschriftung', ['An', 'Aus'], toggle=True)
 
