dfcc8f4296e2043e93048ca603ed5ea361e69e86
stebuehler <stibe.buehler@gmail.com>
2023-10-27 21:19:17 +0200

    Added toggle for violin plot to boxplot tab

--- views/view_box.py
+++ views/view_box.py
@@ -11,6 +11,7 @@ class ViewBox(AbstractView):
         self.add_display_option('x-Achse', ['Jahr', 'Kontinent', 'Sprache gruppiert', 'Baujahr Jahrzehnt', 'Nationalität', 'Bewertungskategorien'])
         self.add_display_option('y-Achse', NUMERICAL_VARIABLES + ['Startzeit normalisiert', 'Dauer (m)', 'Jahr', 'Baujahr (<1950 in 1950 enthalten)'])
         self.add_display_option('Alle Punkte anzeigen', ['An', 'Aus'], toggle=True, default_selection=1)
+        self.add_display_option('Geigengrafik', ['An', 'Aus'], toggle=True, default_selection=1)
 
     def generate_fig(self, opnrcd_df, normalized_time_series, time_series_by_year, **kwargs):
         # retrieve display options
@@ -18,6 +19,8 @@ class ViewBox(AbstractView):
         y_axis_name = kwargs[self.get_display_option_id('y-Achse')]
         y_axis_name = 'Baujahr mapped' if y_axis_name == 'Baujahr (<1950 in 1950 enthalten)' else y_axis_name
         points_bool = kwargs[self.get_display_option_id('Alle Punkte anzeigen')]
+        violin_bool = kwargs[self.get_display_option_id('Geigengrafik')]
+        violin = True if violin_bool == 'An' else False
         points = 'all' if points_bool == 'An' else False
         # the case where all categories are shown at once needs to be treated separately
         categories_ax_x_axis = True if x_axis_name == 'Bewertungskategorien' else False
@@ -25,12 +28,21 @@ class ViewBox(AbstractView):
             x_axis_name = 'Bewertungskategorie'
             y_axis_name = 'Wert'
         df = self.get_df(opnrcd_df, pivot_for_numerical_variables=categories_ax_x_axis).sort_values(by=x_axis_name)
-        self.fig = px.box(
-            df,
-            x=x_axis_name,
-            y=y_axis_name,
-            points=points,
-            )
+        if violin:
+            self.fig = px.violin(
+                df,
+                x=x_axis_name,
+                y=y_axis_name,
+                points=points,
+                box=True,
+                )
+        else:
+            self.fig = px.box(
+                df,
+                x=x_axis_name,
+                y=y_axis_name,
+                points=points,
+                )
         self.fig.update_layout(transition_duration=200)
 
     def get_df(self, df, pivot_for_numerical_variables=False):
